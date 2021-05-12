d988d69603205fcd59e27919311e73fd2b3447c9
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2021-05-12 19:15:45 +0300

    Improve build script

--- build.sh
+++ build.sh
@@ -1,13 +1,46 @@
-#!/bin/sh
+#!/usr/bin/env bash
 set -euo pipefail
 
 NIXPKGS=channel:nixos-20.09
 
-drv="$(nix-build --no-out-link -I nixpkgs="${NIXPKGS}" -E 'let pkgs = import <nixpkgs> {}; in pkgs.callPackage ./packages/kernel/linux-mbp {}')"
+packages=(
+	kernel/linux-mbp
+)
 
-echo ">>> Built '${drv}'"
-du -sh "${drv}"
+modules=(
+	apple-bce
+	apple-ib-drv
+)
 
-if [ -n "${drv}" ] && [ -n "${CACHIX_AUTH_TOKEN}" ] && [ -n "${CACHIX_SIGNING_KEY}" ]; then
-        cachix push t2linux "${drv}"
+built_packages=()
+built_modules=()
+
+for p in ${packages[@]}; do
+	echo ">>> Building '${p}'"
+
+	drv="$(nix-build --no-out-link -I nixpkgs="${NIXPKGS}" -E 'let pkgs = import <nixpkgs> {}; in pkgs.callPackage ./packages/'"${p}"' {}')"
+	built_packages+=(${drv})
+
+	echo ">>> Built '${p}' (-> '${drv}')"
+	du -sh "${drv}"
+done
+
+for p in ${modules[@]}; do
+	echo ">>> Building '${p}'"
+
+	drv="$(nix-build --no-out-link -I nixpkgs="${NIXPKGS}" -E 'let pkgs = import <nixpkgs> {}; mbp = pkgs.callPackage ./packages/kernel/linux-mbp {}; in pkgs.callPackage ./packages/kernel-modules/'"${p}"' { kernel = mbp; }')"
+	built_packages+=(${drv})
+
+	echo ">>> Built '${p}' (-> '${drv}')"
+	du -sh "${drv}"
+done
+
+if [ -n "${CACHIX_AUTH_TOKEN}" ] && [ -n "${CACHIX_SIGNING_KEY}" ]; then
+	for d in ${built_packages[@]}; do
+		cachix push t2linux "${d}"
+	done
+
+	for d in ${built_modules[@]}; do
+		cachix push t2linux "${d}"
+	done
 fi
